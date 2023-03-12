2da1483ddb5ad919f17f03204e4e897df48b7f9d
Zhu Weiji <zhuweiji1997@gmail.com>
2023-03-13 04:46:47 +0800

    added openapi integration on the backend

--- compiler-server-service/main.py
+++ compiler-server-service/main.py
@@ -10,6 +10,7 @@ from pathlib import Path
 from compiler_server_service.routers import (
     cpp_handlers,
     notebook_handlers,
+    openai_handlers,
     tutorial_handlers,
     user_handlers,
 )
@@ -46,6 +47,8 @@ app.include_router(cpp_handlers.router)
 app.include_router(tutorial_handlers.router)
 app.include_router(user_handlers.router)
 app.include_router(notebook_handlers.router)
+app.include_router(openai_handlers.router)
+
 
 
 
