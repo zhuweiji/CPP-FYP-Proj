1eef2a0892c9a2751921697423d75b446a2a7ef4
Zhu Weiji <zhuweiji1997@gmail.com>
2023-03-18 16:29:58 +0800

    changed backend url

--- frontend/src/services/settings.js
+++ frontend/src/services/settings.js
@@ -1,5 +1,5 @@
 export default class SETTINGS {
-    static HOST_URL = "http://18.141.204.202"
+    static HOST_URL = "https://18.141.204.202/"
     // static HOST_URL = "https://p01--compiler-server--m98yzdkgzrwc.code.run/";
     // static HOST_URL = "http://localhost:8080/"
 }
